fc26f1959df745636047962222c91e21cdd9940b
MuleHakim <mulukenhakim97@gmail.com>
2022-08-22 08:53:16 -0700

    Task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,63 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
+ * check_cycle - checks whether a list has cycles
+ *
+ * @list: the list to check
+ *
+ * Description: uses Floyd’s Cycle-Finding Algorithm
+ * * -------------------------------
+ * A hash table can also be used:
+ * Traverse the given linked list and put the address of each node in a Hash table.
+ * If you reach a node whose next pointer is NULL, then given linked list doesn't
+ * contain s cycle.
+ * If address of any node already exist in Hash table, it means we are visiting this node
+ * again and linked list contains a cycle.
+ *
+ * Return: 0 if no cycle; 1 if  cycle exists
  */
-size_t print_listint(const listint_t *h)
+int check_cycle(listint_t *list)
 {
-	const listint_t *current;
-	unsigned int n; /* number of nodes */
+	listint_t *tmp;
+	listint_t *head = tmp = list;
 
-	current = h;
-	n = 0;
-	while (current != NULL)
+	while (head && tmp && tmp->next)
 	{
-		printf("%i\n", current->n);
-		current = current->next;
-		n++;
-	}
-
-	return (n);
-}
-
-/**
- * add_nodeint - adds a new node at the beginning of a listint_t list
- * @head: pointer to a pointer of the start of the list
- * @n: integer to be included in node
- * Return: address of the new element or NULL if it fails
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->next = *head;
-	*head = new;
-
-	return (new);
-}
-
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-	listint_t *current;
-
-	while (head != NULL)
-	{
-		current = head;
 		head = head->next;
-		free(current);
+		tmp = tmp->next->next;
+		if (head == tmp) /* back to where we started */
+		{
+			return (1);
+		}
 	}
+
+	return (0);
 }
