17a350ffe88905b4b70adf868b95b1844d8d3b8b
Muluken Hakim <91600753+MuleHakim@users.noreply.github.com>
2022-09-20 12:13:22 +0300

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
+
 class LockedClass:
-    """ No class or object attributes, can't set
-        Except for first_name
-    """
-    def __setattr__(self, attribute, value):
-        if attribute == "first_name":
-            self.__dict__[attribute] = value
-        else:
-            raise AttributeError("'LockedClass' object has no attribute '" + attribute + "'")
+    """A locked class that only lets the user dynamically create the instance
+    attribute 'first_name'"""
+    __slots__ = ['first_name']
