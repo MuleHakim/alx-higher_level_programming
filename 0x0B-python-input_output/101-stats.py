e4a50a7177ad152c80ac9ff02d6b15e4dc67d113
Muluken Hakim <91600753+MuleHakim@users.noreply.github.com>
2022-09-28 12:20:49 +0300

    Task 14 advanced

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,13 +2,6 @@
 import sys
 import io
 
-#input = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
-#with open(input, "r", encoding="utf-8") as file:
- #   for line in file:
-  #      print(line)
-
-#input = io.TextIOWrapper(sys.stdin , encoding='utf-8')
-
 dictstatus = {}
 totalsize = 0
 totalcount = 0
