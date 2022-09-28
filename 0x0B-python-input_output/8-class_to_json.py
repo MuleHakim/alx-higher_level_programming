d9e1630cf08b3d185dab78fd1bb1833f8342b6e5
Muluken Hakim <91600753+MuleHakim@users.noreply.github.com>
2022-09-28 09:34:58 +0300

    Task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,7 +5,4 @@ Contains the "class_to_json" function
 
 
 def class_to_json(obj):
-    """returns the dictionary description with simple data structure
-    (list, dictionary, string, integer and boolean)
-    for JSON serialization of an object"""
-    return obj.__dict__
+    return vars(obj)
