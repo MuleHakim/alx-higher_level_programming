8f97dcf568d44ab51cb239497df60044d6cb54e7
Muluken Hakim <91600753+MuleHakim@users.noreply.github.com>
2022-11-30 09:02:05 +0300

    Mandatory tasks

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let rev = [];
-  for (let i = 0; i < list.length; i++) {
-    rev.unshift(list[i]);
-  }
-  return rev;
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
+  }, []);
 };
